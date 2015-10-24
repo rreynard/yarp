b54c40e78a647ec688ee370969c76e4a6cecab34
rreynard <marco-sprenger@t-online.de>
2015-10-24 23:04:20 +0200

    Refactor

--- source/yarp.main.js
+++ source/yarp.main.js
@@ -1,9 +1,3 @@
-/**
-
-    defines base for http / Websocket Server
-    CONVENTIONS:
-        - null as standard 0/undefined/false
-**/
 var http = require("http"),
     util = require("./yarp.util.js"),
     YarpLibrary = require("./yarp.library.js"),
