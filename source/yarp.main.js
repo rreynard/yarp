686ff45dbd5b6ec5d8053512c8cb101545418136
rreynard <marco-sprenger@t-online.de>
2015-10-24 22:13:04 +0200

    Refactor

--- source/yarp.main.js
+++ source/yarp.main.js
@@ -4,30 +4,9 @@
     CONVENTIONS:
         - null as standard 0/undefined/false
 **/
-var http = require("http");
-var util = require("./yarp.util.js");
-
-function YarpLibrary() {
-    
-    this.__stor = {
-        lib : {}
-    }
-    
-    this.store = function(key, value) {
-        this.__stor.lib[key] = value;
-        return this;
-    }
-    
-    this.getKey = function(key) {
-        return typeof this.__stor.lib[key] !== "undefined" ? this.__stor.lib[key] : null;
-    }
-    
-    this.remove = function(key) {
-    
-        if(typeof this.__stor.lib[key] !== "undefined") this.__stor.lib[key] = undefined;
-        return this;
-    }
-}
+var http = require("http"),
+    util = require("./yarp.util.js"),
+    YarpLibrary = require("./yarp.library.js");
 
 function AbstractServerObject() {
 
@@ -88,12 +67,5 @@ function AbstractServerObject() {
     }
     return this;
 }
-
-var aso = new AbstractServerObject()
-.conf("port", 1515)
-.on("end", function() {
-    return "Hello World!"
-})
-.run();
 
 module.exports = AbstractServerObject;
