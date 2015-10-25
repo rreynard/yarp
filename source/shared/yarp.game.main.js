d955f44a52b6a9dbf1852dc66038ba91d4401414
rreynard <marco-sprenger@t-online.de>
2015-10-25 23:49:01 +0100

    refactor into yarp.game.entity.js

--- source/shared/yarp.game.main.js
+++ source/shared/yarp.game.main.js
@@ -9,37 +9,4 @@
         - inventory
     
 **/
-// ===== BASE ENTITY =====
-// player character, npc, hostile etc...
-Yarp.Entity.BaseEntity.prototype.traits = {
-    aid : 0, // area id
-    pos : [0,0],
-    equip : {}, // @TODO define
-    states : [] // @TODO define
-};
-
-//===== BASE INVENTORY ====
-var inv = new Yarp.Storage.Inventory();
-console.log(inv);
-inv.addItemsAsync([{id : 2, hp : 10}, {id : 3, hp : 5}, {id : 4, hp : 12}], function() {
-    console.log("finished!");
-    console.log(this.items);
-});
-
-// either
-// inv.indexOfItemAsync({id : 2}, inv.removeItemByIndex);
-
-// or
-inv.removeItemAsync({id : 2});
-
-setTimeout(function() {
-    console.log(inv.items)
-}, 2000)
-
-inv.findItemAsync({id : 4}, function(item) {
-    console.log(item);
-});
-
-if(typeof module !== "undefined" && typeof module["exports"] !== "undefined") {
-
-}
+console.log(GLOBAL);
