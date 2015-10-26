29c98685937b2f401d43028e0fe5a28f8fb6bb8c
rreynard <marco-sprenger@t-online.de>
2015-10-26 23:22:14 +0100

    refactor AbstractStorage

--- source/shared/yarp.game.storage.js
+++ source/shared/yarp.game.storage.js
@@ -1,3 +1,4 @@
+GLOBAL.Yarp.CONSTANTS.STORAGE = {};
 GLOBAL.Yarp.CONSTANTS.STORAGE.ITEM_SLOTS = {
     HEAD : 0,
     CHEST : 1,
@@ -6,55 +7,77 @@ GLOBAL.Yarp.CONSTANTS.STORAGE.ITEM_SLOTS = {
     FEET : 4
 }
 
-Yarp.Storage.BaseStorage.prototype.traits = {  items : [] }
-
-Yarp.Storage.Inventory.prototype.traits = {
-    addItems : function(items) {
+GLOBAL.Yarp.Storage.AbstractStorage.prototype.traits = {  
+    items : [],
+    add : function(items) {
+        if(typeof items !== "object") items = [items];
         for(var i = 0; i < items.length; i++) {
             this.items.push(items[i])
         }
     },
-    addItemsAsync : function(items, cb) {
+    addAsync : function(items, cb) {
         var that = this;
         setTimeout(function() {
-            that.addItems(items);
+            that.add(items);
             if(typeof cb === "function") cb.bind(that)();
         }, 0);
     },
-    removeItemByIndex : function(index) {
+    removeByIndex : function(index) {
         this.items.splice(index, 1);
     },
-    indexOfItem : function(obj) {
+    index : function(obj) {
+        // @TODO define Error Messages
+        if(typeof obj !== "object") throw "Invalid Argument Passed in Yarp.Storage.AbstractStorage.index() typeof argument !== 'object'!";
         var key = Object.keys(obj)[0];
         for(i = 0; i < this.items.length; i++) {
             if(typeof this.items[i][key] !== "undefined" && this.items[i][key] === obj[key]) return i;
         }
         return -1;
     },
-    indexOfItemAsync : function(obj, cb) {
+    indexAsync : function(obj, cb) {
         var that = this;
         setTimeout(function() {
-            if(typeof cb === "function") cb.bind(that)(that.indexOfItem(obj));
+            if(typeof cb === "function") cb.bind(that)(that.index(obj));
         }, 0);
     },
-    removeItem : function(obj) {
-        this.removeItemByIndex(this.indexOfItem(obj));
+    remove : function(obj) {
+        this.removeByIndex(this.index(obj));
+        return this;
     },
-    removeItemAsync : function(obj, cb) {
+    removeAsync : function(obj, cb) {
         var that = this;
         setTimeout(function() {
-            that.removeItem(obj);
+            that.remove(obj);
             if(typeof cb === "function") cb.bind(that)();
         }, 0)
     },
-    findItem : function(obj) {
-        var res = this.indexOfItem(obj);
+    find : function(obj) {
+        if(typeof obj !== "object") throw "Invalid Argument Passed in Yarp.Storage.AbstractStorage.index() typeof argument !== 'object'!";
+        var res = this.index(obj);
         return res !== -1 ? this.items[res] : null;
     },
-    findItemAsync : function(obj, cb) {
+    findAsync : function(obj, cb) {
         var that = this;
         setTimeout(function() {
-            if(typeof cb === "function") cb.bind(that)(that.findItem(obj))
+            if(typeof cb === "function") cb.bind(that)(that.find(obj))
         }, 0)
+    },
+    each : function(fn) {
+        for(var i = 0; i < this.items.length; i++) {
+            fn.bind(this.items[i])(i);
+        }
+        return this;
+    },
+    eachAsync : function(fn, done) {
+        var that = this;
+        setTimeout(function() {
+            that.each(fn);
+            if(typeof done === "function") done();
+        }, 0);
     }
-};
+}
+
+GLOBAL.Yarp.Storage.Inventory.prototype.traits = {};
+if(GLOBAL.Yarp.CONSTANTS.CONTEXT === 0) {
+    console.log("Yarp.Storage done");
+}
